c8d5048811f3053d16c3a5b00783a8e651aec03d
Alvin <alvinvaati@gmail.com>
2023-09-13 13:08:36 +0300

    session Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,13 +40,16 @@ def handle_request():
         access the resource.
     """
     paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-             '/api/v1/forbidden/', '/api/v1/stat*']
+             '/api/v1/forbidden/', '/api/v1/stat*',
+             '/api/v1/auth_session/login/'
+             ]
     if auth is None:
         return
     if not auth.require_auth(request.path, paths):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
+    if auth.authorization_header(request) is None and auth.session_cookie(
+            request) is None:
+        return None, abort(401)
     if auth.current_user(request) is not None:
         request.current_user = auth.current_user(request)
     else:
